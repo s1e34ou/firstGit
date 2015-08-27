6dda7cf4f6aaa86c3606eebb9b659470a9ed9a28
Sang hun <Sang hun@Sanghun-PC>
2015-08-27 09:36:35 +0900

    조원도 올림~!!!!!~!~!~!~!~!~!~!~!~!~!~!~!

--- Project_forGit_sjh/test/Test.java
+++ Project_forGit_sjh/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("최상훈");
 		System.out.println("hi");
 		System.out.println("수정 했습니다~!");
 	}
